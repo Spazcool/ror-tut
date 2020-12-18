99f832064b98f3aac5f0c7b9dae22323b42f6876
Spazcool <douglas.wrightiii@gmail.com>
2020-12-17 21:33:25 -0500

    heroku update

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,24 @@
+class PostsController < ApplicationController
+    def index
+        @content_first = 'yolo index'
+        more_content = 'frolo'
+    end
+
+    def new
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def show
+    end
+
+    def destroy
+    end
+
+    def create
+    end
+end
