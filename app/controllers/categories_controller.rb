7347d033d3db520145efe1b16903f6c73a6bf1cd
Spazcool <douglas.wrightiii@gmail.com>
2021-02-24 21:38:04 -0500

    tried scaffolding, got scared after nuking categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,26 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
+
+  # GET /categories
+  # GET /categories.json
   def index
+    @categories = Category.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @categories}
+    end
   end
 
-  def edit
-  end
-
-  def new
-  end
-
+  # GET /categories/1
+  # GET /categories/1.json
   def show
+    @category = Category.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @category}
+    end
   end
+
 end
